7e4cc49dcc776f3ccc115727450842e82db2e028
KanuX-14 <kanux.dev@gmail.com>
2022-07-04 15:45:47 +0000

    Changed command function

--- src/manager/command.c
+++ src/manager/command.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int executeCommand(char command[], int version)
+char commandBlock[1024];
+FILE *commandFile;
+
+int compareBlocks(char sourceBlock[], char destinationBlock[], char coordinate[])
+{
+    // TODO
+
+    return 0;
+}
+
+int createCommand(char path[], int version)
 {
     // TODO
     
